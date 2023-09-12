00daec99119a3c1a6cf1153829623798a4da46c1
AleksandrKastanie <roof10023@gmail.com>
2023-09-12 15:22:39 +0300

    Created newWebSite for SadkoNewLife(not completed)

---
+++ components/Products/Store/StoreProducts.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="container store-section">
+        <ProductsStoreGrid />
+    </section>
+</template>
+<script setup >
+    const storeProducts = [
+        {
+            id: 1,
+            nameOfProduct:"Dual Sphere",
+            storePic:"/pictures/products/dualsphere/DualSphere-isolated_black_2400px_1.webp",
+            storeText:"Short store discription about product",
+            fullDiscription:"Full Discription for this product",
+            arrayOfPic:[
+                {
+                    id:1,
+                    img:""
+                }
+            ]
+        }
+    ]
+</script>
+<style>
+    .store-section{
+        margin-top: 20px !important; 
+    }
+</style>
